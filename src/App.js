0e42707ff507aabcc6a642e8abdcabe1ae937657
“julian <badjab326@gmail.com>
2022-06-11 14:52:52 -0700

    Component data added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom'
+import Nav from './components/Nav'
 
 
 function App() {
   return (
     <div className="App">
-
+        <Nav />
     </div>
   );
 }
---
+++ src/pages/Stocks.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import StocksData from '../components/StocksData';
+import { Link } from 'react-router-dom';
+
+const Stocks = (props) => {
+    return (
+        <div className="currencies">
+            {StocksData.map((stock, index) => {
+                const { name, symbol, lastPrice, change, high, low, open } = stock;
+                
+                return (
+                    <Link key={index} to={`/stocks/${symbol}`}>
+                    <h2>{name}</h2>
+                    </Link>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Stocks;
