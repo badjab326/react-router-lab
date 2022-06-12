6f7e815619ff80db0e2c6d28216489a8dbbba5a7
“julian <badjab326@gmail.com>
2022-06-12 12:48:23 -0700

    Added all routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router-dom'
 import Nav from './components/Nav'
+import About from './pages/About'
+import Home from './pages/Home'
+import Price from './pages/Price'
+import Stocks from './pages/Stocks'
 
 
 function App() {
   return (
     <div className="App">
         <Nav />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/stocks">
+          <Stocks />
+        </Route>
+        <Route path="/price/:symbol" render={(renderProps) => {
+          return <Price {...renderProps} />
+        }} />
     </div>
   );
 }
